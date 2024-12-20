ed0b7197f13cee3d81be2207be077b0e8908f45b
Vasundhara Maheshwari <vasundharamaheshwari@yahoo.co.in>
2024-12-20 18:27:56 +0530

    trial

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,12 @@
-// Backend/server.js
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-
-// Import routes
-const studentRoutes = require('./routes/studentRoutes');
-const mentorRoutes = require('./routes/mentorRoutes');
-
 const app = express();
 
+require('dotenv').config()
+
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(bodyParser.json());
 
-// Use routes
-app.use('/students', studentRoutes);
-app.use('/mentors', mentorRoutes);
+app.listen(process.env.PORT, () => {
 
-const PORT = 8800;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+})
